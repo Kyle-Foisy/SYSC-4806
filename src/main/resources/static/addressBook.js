d0f66c53114226af01119470d677c82686f28322
Kyle Foisy <kylefoisy@cmail.carleton.ca>
2025-10-26 13:46:59 -0400

    Lab 6 p2 v3

--- src/main/resources/static/addressBook.js
+++ src/main/resources/static/addressBook.js
@@ -1,25 +1,23 @@
 function get_AddressBook() {
     let inputValue = $('#id').val();
-    let neededURL = 'https://localhost:8080/books?id=${inputValue}'
-    neededURL.replace("${inputValue}", inputValue)
+    let $addressBookSelector = $('.addressBookid')
+    let $buddySelector = $('.buddybody')
+    let neededURL = 'https://new-kylefoisyaddressbook-d0b8gwead4ahf3f5.eastus2-01.azurewebsites.net/books?id=${inputValue}'
+    neededURL = neededURL.replace("${inputValue}", inputValue)
+    $addressBookSelector.empty()
+    $buddySelector.empty()
     $.ajax({
         url: neededURL,
         type: 'GET',
     }).then(function(data) {
-        $('.addressBookid').append(neededURL)
-        $('.addressBookid').append(data.id)
+        $addressBookSelector.append(data.id)
         $.each(data.buddies, function(i, buddy) {
-                $('.buddybody').append('<tr>');
-                $('.buddybody').append('<td>' + buddy.name + '</td>');
-                $('.buddybody').append('<td>' + buddy.phonenumber + '</td>');
-                $('.buddybody').append('</tr>');
+            $buddySelector.append('<tr>');
+            $buddySelector.append('<td>' + buddy.name + '</td>');
+            $buddySelector.append('<td>' + buddy.phonenumber + '</td>');
+            $buddySelector.append('</tr>');
             }
         )
     })
 }
 
-$('#search-AddressBook').on('click', get_AddressBook())
-
-$(document).ready(function() {
-
-});
