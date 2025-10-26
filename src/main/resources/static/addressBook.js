024146e32a6cee84db53165b4c0b4578c402491a
Kyle Foisy <kylefoisy@cmail.carleton.ca>
2025-10-26 15:17:19 -0400

    Lab 6 p2 v12 - Fix

--- src/main/resources/static/addressBook.js
+++ src/main/resources/static/addressBook.js
@@ -15,7 +15,7 @@ var book = {
 function get_AddressBook() {
     let inputValue = $('#id').val();
     let $addressBookSelector = $('.addressBookid')
-    let buddyTable = document.getElementById('.buddybody')
+    let buddyTable = document.getElementById('Buddies')
     let neededURL = 'https://new-kylefoisyaddressbook-d0b8gwead4ahf3f5.eastus2-01.azurewebsites.net/books?id=${inputValue}'
     neededURL = neededURL.replace("${inputValue}", inputValue)
     $addressBookSelector.empty()
@@ -26,13 +26,13 @@ function get_AddressBook() {
         data: "book",
         success: function(data) {
             $addressBookSelector.append("Address Book " + data.id)
-                $.each(data.buddies, function(i, buddy){
-                    let row = buddyTable.insertRow(buddyTable.rows.length)
-                    let cell1 = row.insertCell(0)
-                    let cell2 = row.insertCell(1)
-                    cell1.innerHTML= buddy.name
-                    cell2.innerHTML = buddy.phonenumber
-                })
+            $.each(data.buddies, function(i, buddy){
+                let row = buddyTable.insertRow(buddyTable.rows.length)
+                let cell1 = row.insertCell(0)
+                let cell2 = row.insertCell(1)
+                cell1.innerHTML= buddy.name
+                cell2.innerHTML = buddy.phonenumber
+            })
         },
         error: function () {
             $addressBookSelector.append("ERROR")
