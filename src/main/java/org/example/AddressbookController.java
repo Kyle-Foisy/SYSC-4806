d901d67eb4b426b39c99983995e60a9e82bf4711
Kyle Foisy <kylefoisy@cmail.carleton.ca>
2025-10-26 13:10:45 -0400

    Lab 6 p2 v2

--- src/main/java/org/example/AddressbookController.java
+++ src/main/java/org/example/AddressbookController.java
@@ -5,12 +5,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class AddressbookController {
     AddressBookRepository addressBookRepository;
     BuddyInfoRepository buddyInfoRepository;
--- src/main/resources/static/addressBook.js
+++ src/main/resources/static/addressBook.js
@@ -1,21 +1,26 @@
-var AddressBookSearcher = {
-    get_AddressBook: (function() {
-        var inputValue = $('#id').val();
-        var neededURL = 'https://new-kylefoisyaddressbook-d0b8gwead4ahf3f5.eastus2-01.azurewebsites.net/books?id=${inputValue}'
-        neededURL.replace("${inputValue}", inputValue)
-        $.ajax({
-            url: neededURL,
-            type: 'GET',
-        }).then(function(data) {
-        $('.addressBook-id').append(data.id);
-        $('.addressBook-buddies').append(data.content);
-    });
-})}
+function get_AddressBook() {
+    let inputValue = $('#id').val();
+    let neededURL = 'https://new-kylefoisyaddressbook-d0b8gwead4ahf3f5.eastus2-01.azurewebsites.net/books?id=${inputValue}'
+    neededURL.replace("${inputValue}", inputValue)
+    $.ajax({
+        url: neededURL,
+        type: 'GET',
+    }).then(function(data) {
+        $('.addressBookid').append(neededURL)
+        $('.addressBookid').append(data.id)
+        $.each(data.buddies, function(i, buddy) {
+                $('.buddybody').append('<tr>');
+                $('.buddybody').append('<td>' + buddy.name + '</td>');
+                $('.buddybody').append('<td>' + buddy.phonenumber + '</td>');
+                $('.buddybody').append('</tr>');
+            }
+        )
+    })
+}
 
+$('#search-AddressBook').on('click', function (
+))
 
 $(document).ready(function() {
-
-
-
 
 });
