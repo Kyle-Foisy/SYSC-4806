4ceadaa5fd9189457a63f5bf5e6f8d3825326615
Kyle Foisy <kylefoisy@cmail.carleton.ca>
2025-10-26 16:37:09 -0400

    Lab 6 p2 v15

--- src/main/resources/static/addressBook.js
+++ src/main/resources/static/addressBook.js
@@ -12,36 +12,34 @@ const book = {
     buddies: $buddies.val()
 }
 
-const buddyTable = document.getElementById('Buddies')
+// var table = document.getElementById("buddies")
 
 function addRowBuddyTable(name, phonenumber){
-    let row = buddyTable.insertRow(buddyTable.rows.length)
+    let table = document.getElementById("buddies")
+    let row = table.insertRow()
     let cell1 = row.insertCell(0)
     let cell2 = row.insertCell(1)
-    cell1.innerHTML= name
-    cell2.innerHTML = phonenumber
+    cell1.appendChild(document.createTextNode(name))
+    cell2.appendChild(document.createTextNode(phonenumber.toString()))
 }
 
 function get_AddressBook() {
     let inputValue = $('#id').val();
     let $addressBookSelector = $('.addressBookid')
-
+    let $buddySelector = $('.buddybody')
     let neededURL = 'https://new-kylefoisyaddressbook-d0b8gwead4ahf3f5.eastus2-01.azurewebsites.net/books?id=${inputValue}'
     neededURL = neededURL.replace("${inputValue}", inputValue)
     $addressBookSelector.empty()
-    // let body = buddyTable.getElementsByTagName("tbody")
-    // if (body){
-    //     body.innerHTML = ''
-    // }
+
     $.ajax({
         url: neededURL,
         type: 'GET',
         data: book,
         success: function(data) {
             $addressBookSelector.append("Address Book " + data.id)
-            // $.each(data.buddies, function(i, buddy){
-            //     addRowBuddyTable(buddy.name, buddy.phonenumber)
-            // })
+            $.each(data.buddies, function(i, buddy){
+                addRowBuddyTable(buddy.name, buddy.phonenumber)
+            })
         },
         error: function () {
             $addressBookSelector.append("ERROR")
